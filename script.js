dc1554af7e309b1ed818f72ae182e85d1d462e6f
OrcunGokoluk <orcun.gokoluk@outlook.com>
2024-12-08 13:50:47 +0300

    added-digital-countdown

--- script.js
+++ script.js
@@ -14,6 +14,11 @@ const guestIncrementOne=document.querySelector("#guest-incrementor button:nth-ch
 const guestIncrementTwo=document.querySelector("#guest-incrementor button:nth-child(2) p")
 const guestIncrementThree=document.querySelector("#guest-incrementor button:nth-child(3) p")
 
+const startButton=document.getElementById("start-btn");
+const pauseButton=document.getElementById("pause-btn");
+ pauseButton.style.display="none"
+const counterNumber=document.getElementById("counter-number")
+
 console.log(homeIncrementThree)
 homeIncrementOne.addEventListener("click",()=>
 {
@@ -90,3 +95,63 @@ guestIncrementThree.addEventListener("click",()=>
         }
 })
 
+let minutes=9;
+let seconds = 60; //eeminder: you should change the values inside the resetCountDown function too (line 184).
+
+
+let quarterFinishCheck=false;
+
+function resetCountDown()
+{   counterNumber.textContent=`00:00`;   
+    minutes=9;
+    seconds = 60;
+    startButton.style.display="block";
+    pauseButton.style.display="none";
+    quarterFinishCheck=true;
+}
+
+let isPaused=false;
+
+startButton.addEventListener("click",()=>{
+    
+    quarterFinishCheck=false;
+
+    startButton.style.display="none";
+    pauseButton.style.display="block";
+    
+        const countdown = setInterval(() => {
+        
+        if(isPaused==false)
+        {
+            seconds--;
+        
+            if (minutes<=0 && seconds<0) {
+ 
+                clearInterval(countdown);
+                resetCountDown();      
+            }
+
+            else if(seconds<0)
+            {
+                seconds=59;
+        
+                minutes--;
+            }
+        
+            if(minutes>10&&quarterFinishCheck===false)
+            {   
+                
+                seconds<10 ? counterNumber.innerText=`${minutes}:0${seconds}`:counterNumber.innerText=`${minutes}:${seconds}`;
+            }
+            else if(quarterFinishCheck===false){
+                seconds<10 ? counterNumber.innerText=`0${minutes}:0${seconds}`:counterNumber.innerText=`0${minutes}:${seconds}`;
+            }
+            
+        }
+            }, 1000);
+
+} )
+
+pauseButton.addEventListener("click",()=>{
+   isPaused==true? isPaused=false:isPaused=true 
+})
